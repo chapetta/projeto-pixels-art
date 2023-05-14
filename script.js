fd81d4f7418a86b7592c1f7752d059c033b47323
chapetta <cha.petta@hotmail.com>
2023-05-14 16:13:02 -0300

    Done: requisito 7 - clicar nas cores faz com que seja selecionada

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ for (let index = 0; index < 25; index += 1) {
   pixel.className = 'pixel';
   pixelBoard.appendChild(pixel);
 }
+
+const colors = document.querySelectorAll('.color');
+for (let index = 0; index < colors.length; index += 1) {
+  colors[index].addEventListener('click', () => {
+    const selectedColor = document.querySelector('.selected');
+    selectedColor.classList.remove('selected');
+    colors[index].classList.add('selected');
+  });
+}
