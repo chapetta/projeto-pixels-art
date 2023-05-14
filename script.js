2b4ba39c67ec4642f51273f062a163b0e62ce4a3
chapetta <cha.petta@hotmail.com>
2023-05-14 16:39:43 -0300

    clicar em um pixel faz com que seja pintado com a cor selecionada

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ for (let index = 0; index < colors.length; index += 1) {
     colors[index].classList.add('selected');
   });
 }
+
+const pixels = document.querySelectorAll('.pixel');
+
+for (let index = 0; index < pixels.length; index += 1) {
+  pixels[index].addEventListener('click', () => {
+    const selectedColor = document.querySelector('.selected');
+    console.log(selectedColor.style.backgroundColor);
+    pixels[index].style.background = selectedColor.id;
+  });
+}
